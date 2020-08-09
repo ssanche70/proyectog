dd70d8239183314fc0c0574d4320c576ccb0590f
Sebastian Sanchez <sgeilp55@gmail.com>
2020-08-09 16:15:48 -0500

    exito busca pero no encuentra

--- exito.js
+++ exito.js
@@ -1,4 +1,6 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+const Functions  = require('./modulos/funcion');
+var os = require('os');
 
 const isDebug = true;
 (async () => {
@@ -18,11 +20,21 @@ const isDebug = true;
   const page = await context.newPage();
   
   await page.goto('https://www.exito.com/');
-  await page.waitForXPath('//*[@id="header-container"]/div[2]/div[1]/div/div[3]/div/div/div/label/div/input', busqueda);
+  await page.type('#downshift-0-input', busqueda);
   await page.waitFor(200);
-  await page.waitForXPath("//*[@id='header-container']/div[2]/div[1]/div/div[3]/div/div/div/label/div/span/div/button/svg");
-  const [btnBuscar] = await page.$x("//*[@id='header-container']/div[2]/div[1]/div/div[3]/div/div/div/label/div/span/div/button/svg");
+  await page.waitForXPath("//*[@id='header-container']/div[2]/div[1]/div/div[3]/div/div/div[1]/div/label/div");
+  const [btnBuscar] = await page.$x("//*[@id='header-container']/div[2]/div[1]/div/div[3]/div/div/div[1]/div/label/div");
   await btnBuscar.click({clickCount: 1, delay: 100});
+  await page.waitFor(200);
+  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div");
+  const [textName1] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div");
+  var textEstado1 = await Functions.getTextXelement(page, textName1);
+  console.log(textEstado1);
+  await page.waitFor(200);
+  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div");
+  const [textName2] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div");
+  var textEstado2 = await Functions.getTextXelement(page, textName2);
+  console.log(textEstado2);
   //flujoBot();
 
   debugger;
