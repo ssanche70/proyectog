ee68f0bf90a3c22183e79d04cbae4741697674bf
Sebastian Sanchez <sgeilp55@gmail.com>
2020-08-09 16:01:57 -0500

    jumbo creado y funcionando

---
+++ jumbo.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer');
+const Functions  = require('./modulos/funcion');
+var os = require('os');
+
+const isDebug = true;
+(async () => {
+  var busqueda = 'bombillos';
+  //Configuración browser inicial
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox'],
+    slowMo: isDebug ? 100 : 5,
+    defaultViewport: {
+      width: 1200,
+      height: 768
+    }
+  });
+
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+  
+  await page.goto('https://www.tiendasjumbo.co/');
+  await page.type('#nm-custom-autocomplete', busqueda);
+  await page.waitFor(200);
+  await page.waitForXPath("//*[@id='header']/div/div[2]/div[2]/div[1]/div[1]/label");
+  const [btnBuscar] = await page.$x("//*[@id='header']/div/div[2]/div[2]/div[1]/div[1]/label");
+  await btnBuscar.click({clickCount: 1, delay: 100});
+  await page.waitFor(200);
+  await page.waitForXPath("//*[@id='search-results-page']/div[28]/div/div/div[8]/div[2]");
+  const [textName1] = await page.$x("//*[@id='search-results-page']/div[28]/div/div/div[8]/div[2]");
+  var textEstado1 = await Functions.getTextXelement(page, textName1);
+  console.log(textEstado1);
+  await page.waitFor(200);
+  await page.waitForXPath("//*[@id='search-results-page']/div[28]/div/div/div[8]/div[2]");
+  const [textName2] = await page.$x("//*[@id='search-results-page']/div[28]/div/div/div[8]/div[2]");
+  var textEstado2 = await Functions.getTextXelement(page, textName2);
+  console.log(textEstado2);
+  //flujoBot();
+
+  debugger;
+})();
