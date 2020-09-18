859a833b4b90feee4dc59b1c295fd8d0d6d3a319
David Parra <dafepareca@gmail.com>
2020-09-17 22:01:24 -0500

    Modificación exito

--- exito.js
+++ exito.js
@@ -9,7 +9,7 @@ const isDebug = true;
   const browser = await puppeteer.launch({
     headless: false,
     args: ['--no-sandbox'],
-    slowMo: isDebug ? 100 : 5,
+    slowMo: isDebug ? 100 : 20,
     defaultViewport: {
       width: 1200,
       height: 768
@@ -19,18 +19,25 @@ const isDebug = true;
   const context = await browser.createIncognitoBrowserContext();
   const page = await context.newPage();
   
-  
   await page.goto('https://www.exito.com/');
+  await page.waitFor(200);
   await page.type('#downshift-0-input', busqueda);
   await page.keyboard.press("Enter");
   await page.waitFor(200);
-  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[1]/section/a/article/div/div[3]/div/span");
-  const [textName1] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[1]/section/a/article/div/div[3]/div/span");
+  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div/div/div/button");
+  const [opciones] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div/div/div/button");
+  await opciones.click({clickCount: 1, delay: 100});
+  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div/div/div/div/button[5]");
+  const [opciones1] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div/div/div/div/button[5]");
+  var opcionesD = await Functions.getTextXelement(page, opciones1);
+  await page.keyboard.press("Enter");
+  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[1]");
+  const [textName1] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[1]");
   var textEstado1 = await Functions.getTextXelement(page, textName1);
   console.log(textEstado1);
   await page.waitFor(200);
-  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[2]/section/a/article/div/div[3]/div/span");
-  const [textName2] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[9]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[2]/section/a/article/div/div[3]/div/span");
+  await page.waitForXPath("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[2]");
+  const [textName2] = await page.$x("/html/body/div[2]/div/div[1]/div/div[2]/div/div[10]/section/div[2]/div/div[4]/div/div[2]/div/div[2]/div[5]/div/div/div[2]/div[2]");
   var textEstado2 = await Functions.getTextXelement(page, textName2);
   console.log(textEstado2);
   //flujoBot();
